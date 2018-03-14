70f253b60b61d0dab53ebf90302a6f58ff1de5de
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-14 15:03:13 +0000

    Connected to mongodb.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const app = express(), PORT = 3000;
+const app = express(), PORT = 3000, todoRoutes = require('./routes/todos');
+
 
 app.get('/', function(req, res){
   res.send("Hi.");
 })
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(PORT, function(){
   console.log('Server has started.');
