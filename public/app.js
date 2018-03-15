9a813f783c90444cfe07fe054a1378354cfffef9
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-15 12:10:40 +0000

    able to get data from our api and display.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-alert("hi")
+
+$(document).ready(function(){
+  $.getJSON('/api/todos')
+  .then(addTodos)
+})
+
+function addTodos(todos){
+todos.forEach(function(todo) {
+let newTodo = $('<li class = "task">' + todo.name + '</li>');
+$('.list').append(newTodo);
+});
+}
