df20552f918448daaf15159f2b00776406e9ea2f
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-15 13:27:52 +0000

    finished todo app.

--- public/app.js
+++ public/app.js
@@ -7,7 +7,12 @@ $(document).ready(function () {
     if (event.which == 13) createTodo();
   })
 
-  $('.list').on('click', 'span', function () {
+  $('.list').on('click', 'li', function(){
+    updateTodo($(this))
+  })
+
+  $('.list').on('click', 'span', function (event) {
+    event.stopPropagation();
     removeTodo($(this).parent());
   })
 })
@@ -24,6 +29,7 @@ function createTodo() {
 function addTodo(todo) {
   let newTodo = $('<li class = "task">' + todo.name + ' <span>X</span></li>');
   newTodo.data('id', todo._id)
+  newTodo.data('completed', todo.completed)
   if (todo.completed) newTodo.addClass("done");
   $('.list').append(newTodo);
 }
@@ -43,4 +49,19 @@ function removeTodo(todo) {
     .then(function (data) {
       todo.remove();
     })
+}
+
+function updateTodo(todo){
+  let clickedId = todo.data('id');
+  let isDone = todo.data('completed');
+  let updateData = {completed: !isDone}
+  $.ajax({
+    method: 'PUT',
+    url: `/api/todos/${clickedId}`,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    todo.toggleClass('done');
+    todo.data('completed', !isDone)
+  })
 }
